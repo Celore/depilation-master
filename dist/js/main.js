216ec7afc20ef48e4edaed5622488c16d8f9143a
Asya2307 <asya2307@mail.ru>
2021-01-29 10:55:47 +0500

    fix images pathes

--- dist/js/main.js
+++ dist/js/main.js
@@ -151,7 +151,7 @@ $(document).ready(function () {
       balloonContent: 'Это красивая метка'
     }, {
       iconLayout: 'default#image',
-      iconImageHref: '/dist/img/icon/pin.png',
+      iconImageHref: 'img/icon/pin.png',
       iconImageSize: [50, 50],
       iconImageOffset: [-5, -38]
     });
